e3fa091c1788bc026d39b01b49bdd0faded56132
Mariam Tsikhitatrishvili <mariam.tsikhitatrishvili@emoney.ge>
2022-12-16 15:22:35 +0400

    changes

--- src/components/ProductDesc.js
+++ src/components/ProductDesc.js
@@ -1,12 +1,51 @@
 function ProductDesc({ product }) {
+  const desc = product?.desc.split(":")
+  console.log(product)
+
   return (
     product && <div
-      className={`text-white mt-4 p-4 w-full overflow-y-auto`}
+      className={`text-black bg-white rounded-b-xl mt-4 w-full overflow-y-auto`}
       style={{
-        'backgroundColor': `${product['bg_image URL']}`
+        "&:hover": {
+          background: `${product['bg_image URL']}`
+        },
       }}
     >
-      {product?.desc}
+      <img src={process.env.PUBLIC_URL + `/images/${product.title.toLowerCase()}.jpg`} className="w-full" alt="cart" />
+      <div className="px-4">
+        {desc[0].split("ზედა ნოტები")[0]}
+      </div>
+      <div className="mt-2 px-4">
+        <div className="flex items-center gap-1">
+          <div className="w-1 h-1 rounded-full bg-black"></div>
+          <div>ზედა ნოტები:</div>
+          <div>{desc[1]}</div>
+        </div>
+        <div className="flex items-center gap-1">
+          <div className="w-1 h-1 rounded-full bg-black"></div>
+          <div>შუა ნოტები:</div>
+          <div>{desc[2]}</div>
+        </div>
+        <div className="flex items-center gap-1">
+          <div className="w-1 h-1 rounded-full bg-black"></div>
+          <div>ძირითადი ნოტები:</div>
+          <div>{desc[3]}</div>
+        </div>
+      </div>
+      <div className="flex items-center gap-3 px-4 py-2 mt-2 border-t w-fit">
+        <div className="flex items-center gap-2 text-sm">
+          <img src={process.env.PUBLIC_URL + '/images/france-flag.jpg'} className="w-6" alt="cart" />
+          FRENCH PERFUME
+        </div>
+        <div className="flex items-center gap-2 text-sm">
+          <img src={process.env.PUBLIC_URL + '/images/italy-flag.jpg'} className="w-6" alt="cart" />
+          ITALY DESIGN
+        </div>
+        <div className="flex items-center gap-2 text-sm">
+          <img src={process.env.PUBLIC_URL + '/images/polish-flag.jpg'} className="w-6" alt="cart" />
+          MADE IN POLAND
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/ProductSvitcher.js
+++ src/components/ProductSvitcher.js
@@ -1,23 +1,35 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import ProductDesc from "./ProductDesc"
 
 function ProductSvitcher({ subProducts, setActiveProductIndex }) {
     const [activeProduct, setActiveProduct] = useState(0)
+    const [hovered, setHovered] = useState(false)
+    const [hoverStyle, setHoverStyle] = useState({ backgroundColor: 'red' })
+    const [hoverInd, setHoverInd] = useState(null)
+    // let style = ''
+    useEffect(() => {
+        hovered ? setHoverStyle({ backgroundColor: 'white' }) : setHoverStyle({ backgroundColor: 'black' })
+    }, [hovered])
+
     return (
         subProducts && <div className="md:m-4 m-3">
             <div className="z-200 flex flex-wrap">
                 {
                     subProducts.map((item, ind) =>
                         <div
-                            className={`cursor-pointer hover:opacity-70 duration-200 text-black high-caps bg-white border-r border-r-slate-500ß p-2 hover:bg-[${item['bg_image URL']}] text-xl w-[190px] text-center`} key={ind}
+                            className={`cursor-pointer bg-white duration-200 text-black high-caps border-r border-b border-slate-500 p-2 hover:bg-[${item['bg_image URL']}] text-xl w-[190px] text-center`} key={ind}
                             onClick={() => {
                                 setActiveProduct(ind)
                                 setActiveProductIndex(ind)
+                                setHoverInd(ind)
                             }}
-                            // style={{
-                            //     'backgroundColor':`${item['bg_image URL']}B3`
-                            // }}
-                            >
+                            onMouseEnter={() => setHovered(true)}
+                            onMouseLeave={() => setHovered(false)}
+                        // style={{
+                        //     'backgroundColor':`${item['bg_image URL']}B3`
+                        // }}
+                            // style={ind === activeProduct ? { hoverStyle } : { backgroundColor: 'white' }}
+                        >
                             {item.title}
                         </div>
                     )
