1a31f8eed6c9244b55fb558a4971ac99bb91de01
Mariam Tsikhitatrishvili <mariam.tsikhitatrishvili@emoney.ge>
2022-12-21 16:31:58 +0400

    change marign

--- src/components/ProductInfo.js
+++ src/components/ProductInfo.js
@@ -1,6 +1,6 @@
 function ProductInfo({ title, longDesc, shortDesc, flag }) {
   return (
-    <div className="z-100 px-6 border-l-2 border-white text-white md:mx-4 md:my-2 m-3 ">
+    <div className="z-100 px-6 border-l-2 border-white text-white md:mx-4 md:mb-2 mt-4 m-3 ">
       <h2 className="text-4xl font-markBold high_caps">{title}</h2>
       <div className="my-1 text-sm">
         {longDesc},  {shortDesc}
