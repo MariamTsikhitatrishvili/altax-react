a85da26bc278dbba87e54ad303eef9c94d1ea8a5
Mariam Tsikhitatrishvili <mariam.tsikhitatrishvili@emoney.ge>
2022-12-22 16:15:25 +0400

    mobile fixes

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,8 +5,8 @@ function Nav() {
         <div className='pl-5 flex justify-between pt-5'>
             <img src={process.env.PUBLIC_URL + '/images/logo.png'} alt="logo" className='w-20' />
             <div className='flex items-center font-markBold'>
-                <div className='md:text-2xl text-lg bg-white md:px-8 px-4 py-4 border-r'>ჩვენს შესახებ</div>
-                <div className='md:text-2xl text-lg bg-white md:px-8  px-4 py-4 border-r'>სად შევიძინო?</div>
+                <div className='md:text-2xl text-base bg-white md:px-8 px-4 py-4 border-r'>ჩვენს შესახებ</div>
+                <div className='md:text-2xl text-base bg-white md:px-8  px-4 py-4 border-r'>სად შევიძინო?</div>
             </div>
         </div>
     )
