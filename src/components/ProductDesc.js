dedff3172e97a82e54290f8210d0cfe3351379f3
MARIAM TSIKHITATRISHVILI <mariam.tsikhitatrishvili@emoney.ge>
2023-02-24 02:04:25 -0800

    change styles

--- src/components/ProductDesc.js
+++ src/components/ProductDesc.js
@@ -1,4 +1,4 @@
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 
 const fadeIn = {
   initial: {
@@ -9,102 +9,131 @@ const fadeIn = {
     // x: 0,
     opacity: 1,
     transition: {
-      duration: 0.3
-    }
+      duration: 0.3,
+    },
   },
-}
+};
 
 function ProductDesc({ product, price, salePrice }) {
-  const desc = product?.desc.split(":")
-  console.log(product)
+  const desc = product?.desc.split(":");
+  console.log(product);
 
   return (
-    product && <div
-      className={`text-black text-sm bg-white rounded-b-xl mt-4 w-full relative overflow-y-auto flex flex-col justify-between min-h-[450px]`}
-    >
-      <img src={process.env.PUBLIC_URL + `/images/Ornament-min.png`} className="w-full h-[100%] absolute top-0 left-0 opacity-90 object-cover z-10" alt="bg" />
-      <div className="flex flex-col  overflow-hidden" >
-        <div className="flex justify-between items-center px-4 z-50">
-          <div className="w-3/4 flex gap-5">
-            <motion.div key={product.title} variants={fadeIn} initial="initial" animate="animate">
-              <img src={process.env.PUBLIC_URL + `/images/${product.title.toLowerCase()}.jpg`} className="mb-2 rounded-b-xl z-20" alt="cart" />
-            </motion.div>
+    product && (
+      <div
+        className={`text-black text-sm bg-white rounded-b-xl mt-4 w-full relative overflow-y-auto flex flex-col justify-between min-h-[450px]`}
+      >
+        <img
+          src={process.env.PUBLIC_URL + `/images/Ornament-min.png`}
+          className="w-full h-[100%] absolute top-0 left-0 opacity-90 object-cover z-10"
+          alt="bg"
+        />
+        <div className="flex flex-col  overflow-hidden">
+          <div className="flex justify-between items-center px-4 z-50">
+            <div className="w-3/4 flex gap-5">
+              <motion.div
+                key={product.title}
+                variants={fadeIn}
+                initial="initial"
+                animate="animate"
+              >
+                <img
+                  src={
+                    process.env.PUBLIC_URL +
+                    `/images/${product.title.toLowerCase()}.jpg`
+                  }
+                  className="mb-2 rounded-b-xl z-20"
+                  alt="cart"
+                />
+              </motion.div>
 
-            <div className="mt-5 flex flex-col gap-1 text-xs whitespace-nowrap">
-              <div>
-                მოდელი:{salePrice?.split(" ")[0]}
-              </div>
-              <div>
-                შტრიხკოდი: {salePrice?.split(" ")[1]}
-              </div>
-              <div className="flex items-center gap-2">
-                SAFETY DOC:
-                <img src={process.env.PUBLIC_URL + `/images/pdf-icon.png`} className="w-6" alt="cart" />
+              <div className="mt-5 flex flex-col gap-1 text-xs whitespace-nowrap">
+                <div>მოდელი:{salePrice?.split(" ")[0]}</div>
+                <div>შტრიხკოდი: {salePrice?.split(" ")[1]}</div>
+                <div className="flex items-center gap-2">
+                  SAFETY DOC:
+                  <img
+                    src={process.env.PUBLIC_URL + `/images/pdf-icon.png`}
+                    className="w-6"
+                    alt="cart"
+                  />
+                </div>
               </div>
             </div>
-          </div>
 
-          <div className="flex flex-col items-end gap-2">
-            {/* {salePrice && <div className="bg-red-700 text-white px-3 py-1">Sale</div>} */}
-            <div className="flex gap-3 items-center font-markLight mr-5">
-              {/* {
+            <div className="flex flex-col items-end gap-2">
+              {/* {salePrice && <div className="bg-red-700 text-white px-3 py-1">Sale</div>} */}
+              <div className="flex gap-3 items-center font-markLight mr-5">
+                {/* {
                 salePrice && <div className="lg:text-3xl text-lg text-red-700 flex gap-3 items-center">
                   {salePrice} ₾
                   <div className="h-7 w-[1px] bg-slate-500"> </div>
                 </div>
               } */}
-              <p className="lg:text-3xl whitespace-nowrap text-lg text-gray-600">
-                {price} ₾
-              </p>
+                <p className="lg:text-3xl whitespace-nowrap text-lg text-gray-600 p-2 bg-white rounded-lg">
+                  {price} ₾
+                </p>
+              </div>
             </div>
           </div>
+          <div className="px-4 mt-4">{desc[0].split("ზედა ნოტები")[0]}</div>
+          <div className="mt-2 px-4 pb-2">
+            <div className="flex gap-2 items-start">
+              <div className="flex items-center gap-1">
+                <div className="w-1 h-1 rounded-full bg-black"></div>
+                <div className="whitespace-nowrap font-bold">ზედა ნოტები:</div>
+              </div>
+              <div>{desc[1]}</div>
+            </div>
+            <div className="flex gap-2 items-start">
+              <div className="flex items-center gap-1">
+                <div className="w-1 h-1 rounded-full bg-black"></div>
+                <div className="whitespace-nowrap font-bold">შუა ნოტები:</div>
+              </div>
+              <div>{desc[2]}</div>
+            </div>
+            <div className="flex gap-2 items-start">
+              <div className="flex items-center gap-1">
+                <div className="w-1 h-1 rounded-full bg-black"></div>
+                <div className="whitespace-nowrap font-bold">
+                  ძირითადი ნოტები:
+                </div>
+              </div>
+              <div>{desc[3]}</div>
+            </div>
+          </div>
+        </div>
 
-        </div>
-        <div className="px-4 mt-4">
-          {desc[0].split("ზედა ნოტები")[0]}
-        </div>
-        <div className="mt-2 px-4 pb-2">
-          <div className="flex gap-2 items-start">
-            <div className="flex items-center gap-1">
-              <div className="w-1 h-1 rounded-full bg-black"></div>
-              <div className="whitespace-nowrap font-bold">ზედა ნოტები:</div>
-            </div>
-            <div>{desc[1]}</div>
+        <div className="flex items-center gap-3 px-4 py-2 border-t w-full relative">
+          <div className="w-full h-[100%] bg-white z-10 absolute top-0 left-0"></div>
+          <div className="flex items-center gap-2 text-xs z-30">
+            <img
+              src={process.env.PUBLIC_URL + "/images/france-flag.jpg"}
+              className="w-6"
+              alt="cart"
+            />
+            FRENCH PERFUME
           </div>
-          <div className="flex gap-2 items-start">
-            <div className="flex items-center gap-1">
-              <div className="w-1 h-1 rounded-full bg-black"></div>
-              <div className="whitespace-nowrap font-bold">შუა ნოტები:</div>
-            </div>
-            <div>{desc[2]}</div>
+          <div className="flex items-center gap-2 text-xs z-30">
+            <img
+              src={process.env.PUBLIC_URL + "/images/italy-flag.jpg"}
+              className="w-6"
+              alt="cart"
+            />
+            ITALY DESIGN
           </div>
-          <div className="flex gap-2 items-start">
-            <div className="flex items-center gap-1">
-              <div className="w-1 h-1 rounded-full bg-black"></div>
-              <div className="whitespace-nowrap font-bold">ძირითადი ნოტები:</div>
-            </div>
-            <div>{desc[3]}</div>
+          <div className="flex items-center gap-2 text-xs z-30">
+            <img
+              src={process.env.PUBLIC_URL + "/images/polish-flag.jpg"}
+              className="w-6"
+              alt="cart"
+            />
+            MADE IN POLAND
           </div>
         </div>
       </div>
-
-      <div className="flex items-center gap-3 px-4 py-2 border-t w-full relative">
-        <div className="w-full h-[100%] bg-white z-10 absolute top-0 left-0"></div>
-        <div className="flex items-center gap-2 text-xs z-30">
-          <img src={process.env.PUBLIC_URL + '/images/france-flag.jpg'} className="w-6" alt="cart" />
-          FRENCH PERFUME
-        </div>
-        <div className="flex items-center gap-2 text-xs z-30">
-          <img src={process.env.PUBLIC_URL + '/images/italy-flag.jpg'} className="w-6" alt="cart" />
-          ITALY DESIGN
-        </div>
-        <div className="flex items-center gap-2 text-xs z-30">
-          <img src={process.env.PUBLIC_URL + '/images/polish-flag.jpg'} className="w-6" alt="cart" />
-          MADE IN POLAND
-        </div>
-      </div>
-    </div>
-  )
+    )
+  );
 }
 
-export default ProductDesc
+export default ProductDesc;
--- src/components/ProductSvitcher.js
+++ src/components/ProductSvitcher.js
@@ -1,86 +1,114 @@
-import { useState, useRef } from "react"
-import ProductDesc from "./ProductDesc"
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation } from 'swiper';
+import { useState, useRef } from "react";
+import ProductDesc from "./ProductDesc";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper";
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
+import "swiper/css";
+import "swiper/css/navigation";
 
+function ProductSvitcher({
+  subProducts,
+  setActiveProductIndex,
+  salePrice,
+  price,
+}) {
+  const [activeProduct, setActiveProduct] = useState(0);
+  const [hovered, setHovered] = useState(false);
+  const [hoverInd, setHoverInd] = useState(null);
 
-function ProductSvitcher({ subProducts, setActiveProductIndex, salePrice, price }) {
-    const [activeProduct, setActiveProduct] = useState(0)
-    const [hovered, setHovered] = useState(false)
-    const [hoverInd, setHoverInd] = useState(null)
+  const prevRef = useRef(null);
+  const nextRef = useRef(null);
+  // let style = ''
+  // useEffect(() => {
+  //     hovered ? setHoverStyle({ backgroundColor: 'white' }) : setHoverStyle({ backgroundColor: 'black' })
+  // }, [hovered])
 
-    const prevRef = useRef(null);
-    const nextRef = useRef(null)
-    // let style = ''
-    // useEffect(() => {
-    //     hovered ? setHoverStyle({ backgroundColor: 'white' }) : setHoverStyle({ backgroundColor: 'black' })
-    // }, [hovered])
-
-    return (
-        subProducts && <div className="md:mx-4 my-2 m-3 ">
-            <div className="flex justify-end w-full gap-3 mb-4">
-                <div ref={prevRef} className=" cursor-pointer hover:border-[#e3000f] rounded-full border-2 duration-200 border-white shadow-sm w-8 h-8 flex items-center justify-center">
-                    <img src={process.env.PUBLIC_URL + `/images/arrow.svg`} className="rotate-180 w-3" alt="arrow" />
-                </div>
-                <div ref={nextRef} className="cursor-pointer hover:border-[#e3000f] rounded-full border-2 duration-200 border-white shadow-sm w-8 h-8 flex items-center justify-center">
-                    <img src={process.env.PUBLIC_URL + `/images/arrow.svg`} className="w-3" alt="arrow" />
-                </div>
-            </div>
-            <Swiper
-                modules={[Navigation]}
-                spaceBetween={0}
-                slidesPerView={6.7}
-                onInit={(swiper) => {
-                    swiper.params.navigation.prevEl = prevRef.current;
-                    swiper.params.navigation.nextEl = nextRef.current;
-                    swiper.navigation.init();
-                    swiper.navigation.update();
-                }}
-                onSlideChange={() => console.log('slide change')}
-                onSwiper={(swiper) => console.log(swiper)}
-                breakpoints={{
-                    0: {
-                        slidesPerView: 3.5,
-                    },
-                    600: {
-                        slidesPerView: 4.5,
-                    },
-                    1024: {
-                        slidesPerView: 6.7,
-                    }
-                }}
-            >
-                {
-                    subProducts.map((item, ind) =>
-                        <SwiperSlide
-                            className={`cursor-pointer bg-white duration-200 text-black high-caps border border-slate-300 p-2 text-sm w-[130px] text-center`} key={ind}
-                            onClick={() => {
-                                setActiveProduct(ind)
-                                setActiveProductIndex(ind)
-                            }}
-                            onMouseEnter={() => {
-                                setHovered(true)
-                                setHoverInd(ind)
-                            }}
-                            onMouseLeave={() => setHovered(false)}
-                            style={{
-                                backgroundColor: hovered && ind === hoverInd || ind === activeProduct ? `${item['bg_image URL']}` : 'white',
-                                color: hovered && ind === hoverInd || ind === activeProduct ? `white` : 'black',
-                                // borderColor: hovered && ind === hoverInd || ind === activeProduct ? `${item['bg_image URL']}B3` : 'gray',
-                            }}
-                        >
-                            {item.title}
-                        </SwiperSlide>
-                    )
-                }
-            </Swiper>
-            <ProductDesc price={price} salePrice={salePrice} product={subProducts[activeProduct]} />
+  return (
+    subProducts && (
+      <div className="md:mx-4 my-2 m-3 ">
+        <div className="flex justify-end w-full gap-3 mb-4">
+          <div
+            ref={prevRef}
+            className=" cursor-pointer hover:bg-[#e3000f] rounded-full border-2 duration-200 border-white shadow-sm w-8 h-8 flex items-center justify-center"
+          >
+            <img
+              src={process.env.PUBLIC_URL + `/images/arrow.svg`}
+              className="rotate-180 w-3"
+              alt="arrow"
+            />
+          </div>
+          <div
+            ref={nextRef}
+            className="cursor-pointer hover:bg-[#e3000f] rounded-full border-2 duration-200 border-white shadow-sm w-8 h-8 flex items-center justify-center"
+          >
+            <img
+              src={process.env.PUBLIC_URL + `/images/arrow.svg`}
+              className="w-3"
+              alt="arrow"
+            />
+          </div>
         </div>
-
+        <Swiper
+          modules={[Navigation]}
+          spaceBetween={0}
+          slidesPerView={6.7}
+          onInit={(swiper) => {
+            swiper.params.navigation.prevEl = prevRef.current;
+            swiper.params.navigation.nextEl = nextRef.current;
+            swiper.navigation.init();
+            swiper.navigation.update();
+          }}
+          onSlideChange={() => console.log("slide change")}
+          onSwiper={(swiper) => console.log(swiper)}
+          breakpoints={{
+            0: {
+              slidesPerView: 3.5,
+            },
+            600: {
+              slidesPerView: 4.5,
+            },
+            1024: {
+              slidesPerView: 6.7,
+            },
+          }}
+        >
+          {subProducts.map((item, ind) => (
+            <SwiperSlide
+              className={`cursor-pointer bg-white duration-200 text-black high-caps border border-slate-300 p-2 text-sm w-[130px] text-center`}
+              key={ind}
+              onClick={() => {
+                setActiveProduct(ind);
+                setActiveProductIndex(ind);
+              }}
+              onMouseEnter={() => {
+                setHovered(true);
+                setHoverInd(ind);
+              }}
+              onMouseLeave={() => setHovered(false)}
+              style={{
+                backgroundColor:
+                  (hovered && ind === hoverInd) || ind === activeProduct
+                    ? `${item["bg_image URL"]}`
+                    : "white",
+                color:
+                  (hovered && ind === hoverInd) || ind === activeProduct
+                    ? `white`
+                    : "black",
+                // borderColor: hovered && ind === hoverInd || ind === activeProduct ? `${item['bg_image URL']}B3` : 'gray',
+              }}
+            >
+              {item.title}
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        <ProductDesc
+          price={price}
+          salePrice={salePrice}
+          product={subProducts[activeProduct]}
+        />
+      </div>
     )
+  );
 }
 
-export default ProductSvitcher
+export default ProductSvitcher;
